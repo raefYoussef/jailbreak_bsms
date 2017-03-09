ec5fd65e2d73d2fe92095328228822e7206e650b
raefYoussef <raef1@umbc.edu>
2017-03-09 00:53:36 -0500

    Added a sign in page (front end)

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, flash, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    return "Hi there, how you doing?"
+@app.route('/', )
+def signin():
+	return render_template("signin.html", methods=['POST'])
 
 
 if __name__ == "__main__":
