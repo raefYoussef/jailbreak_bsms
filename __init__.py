fdbe220080223f21b88a599488bae16a20e196c5
raefYoussef <raef1@umbc.edu>
2017-03-26 02:50:34 -0400

    Added Dashboard Template II 1. Removed old cached files.

--- __init__.py
+++ __init__.py
@@ -6,35 +6,35 @@ app = Flask(__name__)
 def signin():
 	return render_template("signin.html", methods=['POST'])
 
-@app.route('/home/')
-def home():
-	return render_template("home.html")
-
 @app.route('/signin_temp/', methods=["GET","POST"])
 def signin_temp():
 	error = ''
 	try:
-	    if request.method == "POST":
-	        attempted_username = request.form['username']
-		attempted_password = request.form['password']
-		flash(attempted_username)
-		flash(attempted_password)
+		if request.method == "POST":
+			attempted_username = request.form['username']
+			attempted_password = request.form['password']
+			flash(attempted_username)
+			flash(attempted_password)
 		
-		if attempted_username == "admin" and attempted_password == "password":
-		    return redirect(url_for('home'))
-		else:
-		    error = "Invalid credentials. Try Again."
+			if attempted_username == "admin" and attempted_password == "password":
+				return redirect(url_for('home'))
+			else:
+				error = "Invalid credentials. Try Again."
 
-	    return render_template("signin_temp.html", error = error)		
+		return render_template("signin_temp.html", error = error)		
 	    
 	except Exception as e:
-	    flash(e)
-	    return render_template("signin_temp.html", error = error)
+		flash(e)
+		return render_template("signin_temp.html", error = error)
 
 
 #@app.errorhandler(405)
 #def method_not_found(e):
 #	return render_template("405.html")
+
+@app.route('/home/')
+def home():
+	return render_template("home.html")
 
 @app.route('/dashboard/')
 def dashboard():
