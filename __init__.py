3ce6687ec8ab32414b9314d7fb3b355c59028e01
raefYoussef <raef1@umbc.edu>
2017-05-02 18:20:59 -0400

    Sessions are employed to control access to webapp pages

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, jsonify
+from flask import Flask, render_template, request, url_for, flash, redirect, jsonify, session
 from passlib.hash import sha256_crypt
 from dbconnect import connection
 from MySQLdb import escape_string as thwart
@@ -6,19 +6,6 @@ import gc, datetime
 
 
 app = Flask(__name__)
-
-
-@app.route('/signUp/', methods=["GET","POST"])
-def signUp():
-        return render_template('signUp.html')
-
-
-@app.route('/signUpUser/', methods=["GET","POST"])
-def signUpUser():
-        user =  request.form['username']
-        password = request.form['password']
-
-        return jsonify({'status':'OK','user':user,'pass':password})
 
 
 @app.route('/', methods=["GET","POST"])
@@ -36,7 +23,9 @@ def signin():
 			user_password = data["password"]
 
 			if data_count and sha256_crypt.verify(attempted_password, user_password):
-				
+				session["uid"] = data["uid"]
+				session["username"] = data["username"]
+
 				return redirect(url_for('dashboard'))
 			else:
 				error = "Invalid credentials. Try Again."
@@ -73,34 +62,38 @@ def add_epc():
 		return jsonify({"error": e})
 
 
+
 @app.route('/signin_temp/', methods=["GET","POST"])
 def signin_temp():
-	error = ''
-	try:
-		c, conn = connection()
-		if request.method == "POST":
-			attempted_username = request.form['username']
-			attempted_password = request.form['password']
+	if "uid" in session:
+		error = ''
+		try:
+			c, conn = connection()
+			if request.method == "POST":
+				attempted_username = request.form['username']
+				attempted_password = request.form['password']
 
-			data = c.execute("SELECT * FROM users WHERE username = ('"+ attempted_username + "')")
+				data = c.execute("SELECT * FROM users WHERE username = ('"+ attempted_username + "')")
 
-			#data = c.fetchone()[2]
+				#data = c.fetchone()[2]
 
 
-			if data and sha256_crypt.verify(request.form['password'], c.fetchone()["password"]):
-				#flash(attempted_username)
-				#flash(attempted_password)
-				#if attempted_username == "admin" and attempted_password == "password":
-				return redirect(url_for('dashboard'))
-			else:
-				error = "Invalid credentials. Try Again."
-				gc.collect()
+				if data and sha256_crypt.verify(request.form['password'], c.fetchone()["password"]):
+					#flash(attempted_username)
+					#flash(attempted_password)
+					#if attempted_username == "admin" and attempted_password == "password":
+					return redirect(url_for('dashboard'))
+				else:
+					error = "Invalid credentials. Try Again."
+					gc.collect()
 
-		return render_template("signin_temp.html", error = error)		
-	    
-	except Exception as e:
-		flash(e)
-		return render_template("signin_temp.html", error = error)
+			return render_template("signin_temp.html", error = error)		
+		    
+		except Exception as e:
+			flash(e)
+			return render_template("signin_temp.html", error = error)
+
+	return redirect(url_for('signin'))
 
 
 #@app.errorhandler(405)
@@ -110,37 +103,43 @@ def signin_temp():
 
 @app.route('/home/')
 def home():
-	return render_template("home.html")
+	if "uid" in session:
+		return render_template("home.html")
+
+	return redirect(url_for('signin'))
 
 
 @app.route('/dashboard/')
 def dashboard():
-	return render_template("dashboard.html", account_name = "Kasey Turner")
+	if "uid" in session:
+		return render_template("dashboard.html", account_name = session["username"])
+
+	return redirect(url_for('signin'))
 
 
 @app.route('/dashboard_ajax_brands/', methods=["POST"])
 def dashboard_ajax_brand():
-		try:
-			# establish a connection to database
-			c, conn = connection()
-			
-			# Beer Brands 
-			brand_count = c.execute("""	SELECT beer_brands.name FROM beer_brands 
-										INNER JOIN inventory ON beer_brands.id = inventory.beer_brand 
-										WHERE ((inventory.status <=> 'FULL_INV' OR inventory.status <=> 'FULL_TAP') AND (inventory.beer_brand IS NOT NULL)) 
-										ORDER BY inventory.beer_brand ASC """) 
-			brands = c.fetchall()
-			# populate set with beer brands frequencies 
-			freq_set = {i["name"]: brands.count(i) for i in brands}
-			# format set into array required by morrisjs donut chart
-			freq_list = [{"label": freq_set.keys()[i], "value": freq_set.values()[i]} for i in range(len(freq_set))]
+	try:
+		# establish a connection to database
+		c, conn = connection()
+		
+		# Beer Brands 
+		brand_count = c.execute("""	SELECT beer_brands.name FROM beer_brands 
+									INNER JOIN inventory ON beer_brands.id = inventory.beer_brand 
+									WHERE ((inventory.status <=> 'FULL_INV' OR inventory.status <=> 'FULL_TAP') AND (inventory.beer_brand IS NOT NULL)) 
+									ORDER BY inventory.beer_brand ASC """) 
+		brands = c.fetchall()
+		# populate set with beer brands frequencies 
+		freq_set = {i["name"]: brands.count(i) for i in brands}
+		# format set into array required by morrisjs donut chart
+		freq_list = [{"label": freq_set.keys()[i], "value": freq_set.values()[i]} for i in range(len(freq_set))]
 
-			return jsonify({"brands_chart":freq_list})
+		return jsonify({"brands_chart":freq_list})
 
-		except Exception as e:
-			# failed connection
-			return jsonify(e)
-
+	except Exception as e:
+		# failed connection
+		return jsonify(e)
+	
 
 @app.route('/dashboard_ajax_customers/', methods=["POST"])
 def dashboard_ajax_customers():
@@ -209,87 +208,97 @@ def dashboard_ajax_kegStatus():
 
 @app.route('/dashboard_ajax_notifications/', methods=["POST"])
 def dashboard_ajax_notifications():
-		try:
-			# establish a connection to database
-			c, conn = connection()
-			
-			# Shipped Kegs
-			shipped_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, activity_log.customer, users.username AS user FROM activity_log 
-											INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
-											LEFT JOIN users ON activity_log.user=users.Uid 
-											WHERE activity_log.status="FULL_OUT" AND activity_log.time >= SUBDATE(NOW(),7) 
-											ORDER BY activity_log.time DESC """) 
-			shipped = c.fetchall()
+	try:
+		# establish a connection to database
+		c, conn = connection()
+		
+		# Shipped Kegs
+		shipped_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, activity_log.customer, users.username AS user FROM activity_log 
+										INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
+										LEFT JOIN users ON activity_log.user=users.Uid 
+										WHERE activity_log.status="FULL_OUT" AND activity_log.time >= SUBDATE(NOW(),7) 
+										ORDER BY activity_log.time DESC """) 
+		shipped = c.fetchall()
 
-			shipped_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["customer"], i["user"]] for i in shipped]
-
-
-			# Filled Kegs
-			filled_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, users.username AS user FROM activity_log 
-											INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
-											LEFT JOIN users ON activity_log.user=users.Uid 
-											WHERE activity_log.status="FULL_INV" AND activity_log.time >= SUBDATE(NOW(),7) 
-											ORDER BY activity_log.time DESC """) 
-			filled = c.fetchall()
-
-			filled_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["user"]] for i in filled]
+		shipped_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["customer"], i["user"]] for i in shipped]
 
 
-			# Tapped Kegs
-			tapped_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, users.username AS user FROM activity_log 
-											INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
-											LEFT JOIN users ON activity_log.user=users.Uid 
-											WHERE activity_log.status="FULL_TAP" AND activity_log.time >= SUBDATE(NOW(),7) 
-											ORDER BY activity_log.time DESC """) 
-			tapped = c.fetchall()
+		# Filled Kegs
+		filled_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, users.username AS user FROM activity_log 
+										INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
+										LEFT JOIN users ON activity_log.user=users.Uid 
+										WHERE activity_log.status="FULL_INV" AND activity_log.time >= SUBDATE(NOW(),7) 
+										ORDER BY activity_log.time DESC """) 
+		filled = c.fetchall()
 
-			tapped_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["user"]] for i in tapped]
-
-
-			# Returned Kegs
-			returned_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, activity_log.customer, users.username AS user FROM activity_log 
-											INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
-											LEFT JOIN users ON activity_log.user=users.Uid 
-											WHERE activity_log.time >= SUBDATE(NOW(),7) AND activity_log.status="DIRTY" AND activity_log.customer IS NOT NULL AND activity_log.beer_brand IS NOT NULL
-											ORDER BY activity_log.time DESC """) 
-			returned = c.fetchall()
-
-			returned_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["customer"], i["user"]] for i in returned]
+		filled_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["user"]] for i in filled]
 
 
+		# Tapped Kegs
+		tapped_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, users.username AS user FROM activity_log 
+										INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
+										LEFT JOIN users ON activity_log.user=users.Uid 
+										WHERE activity_log.status="FULL_TAP" AND activity_log.time >= SUBDATE(NOW(),7) 
+										ORDER BY activity_log.time DESC """) 
+		tapped = c.fetchall()
 
-			return jsonify({"shipped": shipped_list, "shipped_count": shipped_count, "filled": filled_list, "filled_count": filled_count, "tapped": tapped_list, "tapped_count": tapped_count, "returned": returned_list, "returned_count": returned_count})
+		tapped_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["user"]] for i in tapped]
 
-		except Exception as e:
-			# failed connection
-			return e
+
+		# Returned Kegs
+		returned_count = c.execute("""	SELECT activity_log.keg_id, activity_log.time, beer_brands.name AS beer_brand, activity_log.customer, users.username AS user FROM activity_log 
+										INNER JOIN beer_brands ON beer_brands.id = activity_log.beer_brand 
+										LEFT JOIN users ON activity_log.user=users.Uid 
+										WHERE activity_log.time >= SUBDATE(NOW(),7) AND activity_log.status="DIRTY" AND activity_log.customer IS NOT NULL AND activity_log.beer_brand IS NOT NULL
+										ORDER BY activity_log.time DESC """) 
+		returned = c.fetchall()
+
+		returned_list = [[i["keg_id"], str(i["time"]), i["beer_brand"], i["customer"], i["user"]] for i in returned]
+
+
+
+		return jsonify({"shipped": shipped_list, "shipped_count": shipped_count, "filled": filled_list, "filled_count": filled_count, "tapped": tapped_list, "tapped_count": tapped_count, "returned": returned_list, "returned_count": returned_count})
+
+	except Exception as e:
+		# failed connection
+		return e
 
 
 @app.route('/settings/')
 def settings():
-	return render_template("settings.html")
+	if "uid" in session:
+		return render_template("settings.html")
+
+	return redirect(url_for('signin'))
 
 
 @app.route('/view_inventory/', methods=["GET","POST"])
 def view_inventory():
-	c, conn = connection()
-	inventory_items = c.execute("SELECT * FROM inventory")
-	inventory_items = c.fetchall()
-	# x = [{key_id: 1}, {keg_id: 2}, {key_id: 3}]
-	# x[0]["keg_id"]
-	#print("hello")
-	#print(inventory_items)
-	return render_template("view_inventory.html", inventory=inventory_items,  account_name = "Kasey Turner")
+	if "uid" in session:
+		c, conn = connection()
+		inventory_items = c.execute("SELECT * FROM inventory")
+		inventory_items = c.fetchall()
+		# x = [{key_id: 1}, {keg_id: 2}, {key_id: 3}]
+		# x[0]["keg_id"]
+		#print("hello")
+		#print(inventory_items)
+		return render_template("view_inventory.html", inventory=inventory_items,  account_name = session["username"])
 
+	return redirect(url_for('signin'))
 
 @app.route('/edit_inventory/', methods=["GET","POST"])
 def edit_inventory():
-	return render_template("edit_inventory.html", methods=['POST'])
-    
+	if "uid" in session:
+		return render_template("edit_inventory.html", account_name = session["username"])
+
+	return redirect(url_for('signin'))
 
 @app.route('/logistics/')
 def logistics():
-	return render_template("logistics.html", account_name = "Kasey Turner")
+	if "uid" in session:
+		return render_template("logistics.html", methods=['POST'])
+
+	return redirect(url_for('signin'))
 
 
 if __name__ == "__main__":
